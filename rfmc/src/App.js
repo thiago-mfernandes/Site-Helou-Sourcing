9126a58e43af84d4a856611447f3be6c255b0537
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-07-14 16:46:26 -0300

    estruturacao preliminar do html

--- rfmc/src/App.js
+++ rfmc/src/App.js
@@ -1,10 +1,224 @@
 
-function App() {
+export default function App() {
   return (
-    <div>
-      teste
+    <main>
+
+    {/* inicio header e menu */}
+    <section>
+      <header>
+        <img src="" alt="logotipo" />
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Companhia</li>
+            <li>O Que Fazemos?</li>
+            <li>Sobre Nós</li>
+            <li>Contato</li>
+          </ul>
+        </nav>
+        <span>icone mobile</span>
+      </header>
+    </section>
+    <div>{/* aqui vai o background img 100%*/}
+      <div>{/* aqui vai o container com as margens laterais*/}
+        <div>{/* aqui vai o container do titulo e subtitulo*/}
+          <h1>Metais - Ferros - Catalisadores</h1>
+          <p>Qualidade, Confiabilidade e Sustentabilidade</p>
+        </div>
+      </div>
     </div>
+    {/* fim header e menu */}
+    
+
+
+    {/* inicio companhia */}
+    <section>
+      <div>
+        <img src="" alt="companhia" />
+        <div> {/* container */}
+          <span>Quem é a RFCM?</span>
+          <h2>Companhia</h2>
+          <p>Somos uma corretora comercial, com sede em São Paulo, Brasil, trabalhando diligentemente no fornecimento de commodities específicas ou na busca de um mercado para seus produtos, onde estendemos nossa experiência de 30 anos para fornecer as soluções necessárias por meio de nossa rede global.</p>
+          <p>Nossa principal prioridade é estabelecer e manter um relacionamento comercial positivo e de longo prazo com nossos clientes e fornecedores.</p>
+          <p>A RFMC firmou parceria de sucesso com fabricantes renomados e conceituados em todo o mundo, garantindo aos nossos clientes nossa capacidade de fornecer produtos de alta qualidade a preços competitivos, respaldados pelo suporte técnico e metalúrgico necessário.</p>
+          <p>Ao mesmo tempo, estamos comprometidos com nossos fornecedores em fornecer a eles um suporte consistente de mercado.</p>
+          
+        </div>
+      </div>
+    </section>
+    {/* fim companhia */}
+
+
+
+    {/* inicio o que fazemos*/}
+    <section>
+      <div> {/* container */}
+        <section>
+          <div>
+            <h2>O que Fazemos?</h2>
+          </div>
+          <div>
+            <div>
+              <h2><span>01.</span>Trabalhamos com todos os tipos de metais e ligas de ferro.</h2>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h2><span>02.</span>Tambem operamos com sucata metalica em diversas variedade.</h2>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h2><span>03.</span>E temos a disposicao de nossos clientes catalisadores e residuos.</h2>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h2>Negociamos em: </h2>
+          </div>
+          <div>
+            <p><span>Cobalto</span>(Minério/Conc, CoOH, CoSO4)</p>
+          </div>
+          <div>
+            <p><span>Manganês</span>(Minério, Conc, MnSO4)</p>
+          </div>
+          <div>
+            <p><span>Zinco</span>(Minério/Conc, ZnSO4, ZnO)</p>
+          </div>
+          <div>
+            <p><span>Cobre</span>(Minério/Conc, CuSO4, CuO)</p>
+          </div>
+          <div>
+            <p><span>Molibdênio</span>(MoO3 > 57%)</p>
+          </div>
+        </section>
+
+        <section>
+          <h2>Metais Não Ferrosos</h2>
+          <div>
+            <p>E em todos os tipos e formas de produtos intermediários contendo metais não ferrosos  (V, Ni, Co, W, Mo, Cu, Pb, Sn, etc…)  na forma de FINO, ÓXIDOS, LAMAS, METÁLICOS, CATALISADORES GASTO, etc…</p>
+          </div>
+          <div>
+            <img src="" alt="fotos formas nao ferrosos" />
+          </div>
+        </section>
+
+      </div>
+    </section>
+    {/* fim o que fazemos */}
+
+
+
+    {/* inicio porque a rfmc - bgcol cinza aqui */}
+    <section>
+      <div>{/* container */}
+        <div>
+          <h2>porque a rfmc</h2>
+        </div>
+        <div>
+          <p>Somos especializados no comércio internacional de metais e na gestão de resíduos industriais. Acreditamos que nossas ações operacionais de reciclagem de resíduos nos permitem não apenas economizar recursos minerais naturais, mas principalmente proteger o meio ambiente do uso incorreto e do descarte e disposição inadequada dos referidos resíduos.</p>
+          <img src="" alt="imagem sustentabilidade" />
+        </div>
+        <div>
+          <p>Nós da RFMC, acreditamos que para fazer diferente é preciso paixão. Sabemos que não somos perfeitos, mas nos esforçamos para fazer a nossa parte. Venha conhecer mais sobre nosso trabalho e como estamos fazendo a diferença.</p>
+        </div>
+      </div>
+    </section>
+    {/* fim inicio porque a rfmc */}
+
+
+
+    {/* inicio produtos */}
+    <section>
+      <div>{/* container */}
+        <div>
+          <h2>Abaixo estão os  <span>METAIS E LIGAS DE FERRO</span>  que comercializamos:</h2>
+        </div>
+
+        <div>
+          <div>{/* card produto */}
+            <img src="" alt="produto" />
+            <div>
+              <h3>Ferro Silicio</h3>
+              <p>Si &gt; 70 / 75% C &lt; 0,1 / 0,2% Al &lt; 0,5 / 1,5 / 2,0%</p>
+            </div>
+          </div>
+
+          <div>{/* card produto */}
+            <img src="" alt="produto" />
+            <div>
+              <h3>Ferro Silicio</h3>
+              <p>Si &gt; 70 / 75% C &lt; 0,1 / 0,2% Al &lt; 0,5 / 1,5 / 2,0%</p>
+            </div>
+          </div>
+
+          <div>{/* card produto */}
+            <img src="" alt="produto" />
+            <div>
+              <h3>Ferro Silicio</h3>
+              <p>Si &gt; 70 / 75% C &lt; 0,1 / 0,2% Al &lt; 0,5 / 1,5 / 2,0%</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    {/* fim produtos produtos */}
+
+
+    {/* inicio contato */}
+    <section>
+      <div>{/* container */}
+        <div>
+          <p>Para solicitar um orçamento, entre em contato conosco por telefone ou e-mail e forneceremos serviços experientes, profissionais e confiáveis.</p>
+          <h3>Contate-nos</h3>
+          <p>Seu parceiro para materiais estratégicos: Óxidos, Metais, Ferro Ligas e Catalisadores</p>
+        </div>
+
+        <div>
+          <form action="">
+            <h2>Formulario</h2>
+            <div>
+              <label htmlFor=""></label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor=""></label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor=""></label>
+              <input type="text" />
+            </div>
+            <div>
+              <input type="submit" value="Enviar Mensagem"/>
+            </div>
+          </form>
+        </div>
+
+        <div>
+          <p>endereco</p>
+          <p>203 Fake St. Mountain View, San Francisco, California, USA</p>
+          <p>telefone</p>
+          <p>+123 456 789</p>
+          <p>email</p>
+          <p>seuemail@dominio.com</p>
+        </div>
+      </div>
+    </section>
+    {/* fim contato */}
+
+
+    {/* inicio footer */}
+    <footer>
+      <div>{/* container */}
+        <p>incluir apenas um sobre nos</p>
+        <p>incluir um copyright</p>
+      </div>
+    </footer>
+    {/* fim footer */}
+    </main>
   );
 }
 
-export default App;
