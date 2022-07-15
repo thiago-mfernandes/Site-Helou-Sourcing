e820ee7456f4d4388984854487c2f376b10a8be0
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-07-15 17:55:02 -0300

    componente do menu iciado com algumas classes na versao mobile

---
+++ rfmc/src/components/menu/index.jsx
@@ -0,0 +1,60 @@
+import propTypes from 'prop-types';
+import Logo from 'assets/logo_rfmc.png';
+import portuguese from 'data/portugueseVersion.json';
+import english from 'data/englishVersion.json';
+import styles from './Menu.module.scss';
+import { useState } from 'react';
+
+function Menu({ language, setLanguage }) {
+  const [menuState, setMenuState] = useState(false);
+
+  let handleLanguage = language ? portuguese : english;
+
+  return (
+    <section>
+      <header className={styles.header}>
+        <img
+          className={styles.header__img}
+          src={Logo}
+          alt="Logotipo da empresa"
+        />
+
+        <button onClick={() => setLanguage(!language)}>Language</button>
+
+        <div
+          className={'icone mobile - ou icone desktop'}
+          onClick={() => setMenuState(!menuState)}
+        >
+          <div className={`${styles.hamburguer} ${styles.hamburguerIcon}`} />
+          <div className={`${styles.hamburguer} ${styles.hamburguerIcon}`} />
+          <div className={`${styles.hamburguer} ${styles.hamburguerIcon}`} />
+        </div>
+
+        <nav className={
+          menuState ? 
+            `${styles.header__nav} ${styles.menuOpen}` : 
+            `${styles.header__nav} ${styles.menuClose}`
+        }>
+          <button 
+            className={styles.btnCloseMobile}
+            onClick={() => setMenuState(false)}
+          >x</button>
+          <ul className={styles.header__nav___itens}>
+            {handleLanguage.nav.map((item, index) => (
+              <li className={styles.header__nav___item} key={index}>
+                {item.label}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    </section>
+  );
+}
+
+Menu.propTypes = {
+  language: propTypes.bool.isRequired,
+  setLanguage: propTypes.func.isRequired,
+};
+
+export default Menu;
